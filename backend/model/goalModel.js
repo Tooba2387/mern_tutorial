e80164c50a569c3a3ac21b9bc68018ce95e2b0dd
Tooba2387 <l192387@lhr.nu.edu.pk>
2023-09-30 04:15:07 +0500

    start of project

---
+++ backend/model/goalModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const goalSchema = new mongoose.Schema ({
+    user: {
+        type: mongoose.Schema.Types.ObjectId, 
+        required: true, 
+        ref: 'User'
+    },
+    text: {
+        type: String, 
+        required: [true, 'add text value']
+    }
+}, { 
+    timestamps: true
+}
+)
+
+const Goal = mongoose.model('Goal', goalSchema);
+
+module.exports = Goal
